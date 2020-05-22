cdd8d61d491ee9e5cc6bc702ef9096b053de6fae
Janneke <jannekeblok@hotmail.nl>
2020-05-22 11:38:40 +0200

    Puuuush

--- Assignment 5 Kalman Filtering/as5_kf_kalman.m
+++ Assignment 5 Kalman Filtering/as5_kf_kalman.m
@@ -32,11 +32,62 @@ function [MArray, xPrioriArray, xPosterioArray, pPosterioArray] = as5_kf_kalman(
 
 Q = params.Q;
 R = params.R;
-
-
+A = ForwardModel.A;
+B = ForwardModel.B;
+C = ForwardModel.C;
+u = data.u;
+P0 = zeros(2,2);
+x0 = zeros(2,1);
+u0 = u(1,1);
+Z = data.yNoise;
 %% 2) Preallocation (reserving space) of matrices to speed up calculations
+
+% MArray = zeros(2,2,length(u));
+% xPrioriArray = zeros(2,length(u));
+% xPosterioArray = zeros(2,length(u));
+% pPosterioArray = zeros(2,2,length(u));
+% pPrioriArray = zeros(2,2,length(u));
+
+MArray = zeros(length(u),2,2);
+xPrioriArray = zeros(length(u),2);
+xPosterioArray = zeros(length(u),2);
+pPosterioArray = zeros(length(u),2,2);
+pPrioriArray = zeros(length(u),2,2);
+
 
 %% 3) Initialize Covariance and State Estimates
 
+% xPrioriArray(:,1) = A*x0+B*u0;
+% pPrioriArray(:,:,1) = A*P0*A.'+Q;
+
+xPrioriArray(1,:) = A*x0+B*u0;
+pPriori = A*P0*A.'+Q;
+pPrioriArray(1,:,:) = pPriori;
+
 %% 4) Run the Kalman Filter
 
+% for i = 1:(length(u)-1)
+%     MArray(:,:,i) = pPrioriArray(:,:,i)*C.'*(C*pPrioriArray(:,:,i)*C.'+R).^(-1);
+%     xPosterioArray(:,i) = xPrioriArray(:,i)+MArray(:,:,i)*(Z(:,i)-C*xPrioriArray(:,i));
+%     pPosterioArray(:,:,i) = pPrioriArray(:,:,i)-MArray(:,:,i)*C*pPrioriArray(:,:,1);
+%     
+%     xPrioriArray(:,i+1) = A*xPosterioArray(:,i)+B*u(:,i);
+%     pPrioriArray(:,:,i+1) = A*pPosterioArray(:,:,i)*A.'+Q;
+% end
+% 
+for i = 1:(length(u)-1)
+    M = pPriori*C.'/(C*pPriori*C.'+R);
+    MArray(i,:,:) = M;
+    xPosterioArray(i,:) = xPrioriArray(i,:)+(M*(Z(i,:).'-C*xPrioriArray(i,:).')).';
+    pPosterio = pPriori-M*C*pPriori;
+    pPosterioArray(i,:,:) = pPosterio;
+    
+    xPrioriArray(i+1,:) = (A*xPosterioArray(i,:).').'+B.'*u(i,:);
+    pPriori = A*pPosterio*A.'+Q;
+    pPrioriArray(i+1,:,:) = pPriori;
+end
+
+% xPosterioArray = xPosterioArray.';
+% xPrioriArray = xPrioriArray.';
+% MArray = permute(MArray,[3 1 2]);
+% pPosterioArray = permute(pPosterioArray,[3 1 2]);
