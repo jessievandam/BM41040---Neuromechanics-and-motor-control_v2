1e2028d7ddb676bdce59c27da6ac9f6365ed67b1
Janneke <jannekeblok@hotmail.nl>
2020-05-22 09:24:41 +0200

    Push it

--- Assignment 5 Kalman Filtering/as5_kf_dynamics.m
+++ Assignment 5 Kalman Filtering/as5_kf_dynamics.m
@@ -16,7 +16,7 @@ function [TrueHandIdeal, TrueHandNoisy, ForwardModel]=as5_kf_dynamics(params)
 %
 %  TrueHandIdeal:    Discrete State Space Model of the Ideal Hand (without noise)
 %  TrueHandNoisy:    Discrete State Space Model with both Noise Sources (w and v) added to the Ideal Model of the Hand 
-%  ForwardModel:    Discrete State Space Model of Forward Model
+%  ForwardModel:     Discrete State Space Model of Forward Model
 % 
 %
 % Hint:
@@ -37,13 +37,34 @@ function [TrueHandIdeal, TrueHandNoisy, ForwardModel]=as5_kf_dynamics(params)
 
 %% 1) Create a state-space model of the true Hand dynamics
 
+A = [0 1;
+    0 -params.beta/params.m];
+
+B = [0;
+    1/params.m];
+
+C = eye(2);
+
+D = zeros(2,1);
+
+sys = ss(A,B,C,D,'inputname','u','outputname','y');
+
 %% 2) Transform the continuous model into a discrete model.
 
+TrueHandIdeal = c2d(sys,params.dt);
+
 %% 3) Create the forward model of the hand
+
+ForwardModel = TrueHandIdeal;
 
 %% 4) Noisy model
 % Add one additional input to your original model representing the process
 % noise and two inputs for the measurement noise.
 % This comes down to extending the B and D matrix appropriately.
 
+% sys_disc.B = [sys_disc.B [1; 1] [1;1] [0; 0] [0; 0]];
+B_noise = [TrueHandIdeal.B [1 1 0 0; 1 1 0 0]];
+D_noise = [TrueHandIdeal.D [0; 0] [0;0] [1; 1] [1;1]];
+
+TrueHandNoisy = ss(TrueHandIdeal.A,B_noise,TrueHandIdeal.C, D_noise,params.dt);
 
